faf04bbe3ea9f4c4d22dcc765e3b6675b02f2901
pramesh gurav <prameshgurav79286@gmail.com>
2025-04-19 10:35:17 +0530

    Update

---
+++ fifth_app/src/index.js
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom/client';
+import { ShieldPass } from './App';
+
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(<ShieldPass/>)
---
+++ seventh_app/src/User.js
@@ -0,0 +1,9 @@
+export function AvengerUser() {
+    return (
+        <>
+        <h1>Welcome Avenger</h1>
+        <button>Log In</button>
+        <button>Sign Up</button>
+        </>
+    )
+}
